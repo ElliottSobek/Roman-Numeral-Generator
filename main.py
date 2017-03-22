2a667c8ed92dbfe92b686e2c7ba36eec2e7f29b2
Elliott Sobek <elliottsobek@gmail.com>
2017-03-22 12:49:50 -0600

    Add numeral lookup dictionay

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# dictionary of numbers to symbols
 def main():
     try:
         u_in = int(input("Enter a number: "))
     except ValueError:
         print("Invalid input")
         return
+    numeral_lookup = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
+    print(numeral_lookup.get(1))
     print(u_in)
     return
 
