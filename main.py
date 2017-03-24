cddd8d20193d4822cf934035b87cd0dd34bc71f3
Elliott Sobek <elliottsobek@gmail.com>
2017-03-23 19:17:23 -0600

    Implement basic decomposition behaviour for simple numbers

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numeral_lookup = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M', 5000: 'U+2181', 10000: 'U+2182'
-    , 50000: 'U+2187', 100000: 'U+2188'}
+#numeral_lookup = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M', 5000: 'U+2181', 10000: 'U+2182'
+ #   , 50000: 'U+2187', 100000: 'U+2188'}
+
+numeral_lookup = [(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D'), (1000, 'M'), (5000, 'U+2181'),
+                  (10000, 'U+2182'), (50000, 'U+2187'), (100000, 'U+2188')]
 
 # num
 # num // lowest num but bigger, for example 400 is smaller than L but bigger or equal than C; C < 400 < L
@@ -11,14 +14,17 @@ numeral_lookup = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M
 
 def decompose(integer):
     prev = -1
+    pair = ()
     while integer != 0:
-        for key, rom_num in numeral_lookup.items():
-            if key >= integer:
+        for item in numeral_lookup:
+            if item[0] > integer:
                 break
-            prev = key
+            prev = item[0]
+            pair = item
         result = integer // prev
         if result != 0:
-            print(numeral_lookup.get(prev) * result)
+            print(pair[1] * result)
+            integer -= result
             result %= prev
     return
 
