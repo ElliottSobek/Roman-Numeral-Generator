54f484891bd77f6254bb91d6e52def7eeb7974cc
Elliott Sobek <elliottsobek@gmail.com>
2017-03-23 18:29:47 -0600

    Implement initial decompose function

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
+numeral_lookup = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M', 5000: 'U+2181', 10000: 'U+2182'
+    , 50000: 'U+2187', 100000: 'U+2188'}
+
+# num
+# num // lowest num but bigger, for example 400 is smaller than L but bigger or equal than C; C < 400 < L
+# store result in var
+# result * var for ammount of symbols
+# num %= current roman numeral
+# if 0; Done
+
+
+def decompose(integer):
+    prev = -1
+    while integer != 0:
+        for key, rom_num in numeral_lookup.items():
+            if key >= integer:
+                break
+            prev = key
+        result = integer // prev
+        if result != 0:
+            print(numeral_lookup.get(prev) * result)
+            result %= prev
+    return
+
+
 def main():
     try:
         u_in = int(input("Enter a number: "))
     except ValueError:
         print("Invalid input")
         return
-    numeral_lookup = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
-    print(numeral_lookup.get(1))
-    print(u_in)
+    decompose(u_in)
     return
 
 main()
