461366c49bc4dad3ec25d54c7f9ebf3f3905c88e
ElliottSobek <elliottsobek@gmail.com>
2017-04-12 22:55:30 -0600

    Implement initial procedural decomposition logic

--- main.py
+++ main.py
@@ -13,80 +13,11 @@
 
 # test numbers: 3, 4, 6, 39, 73, 124, 578, 9999
 
-numeral_lookup = [(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D'), (1000, 'M'), (5000, 'U+2181'),
-                      (10000, 'U+2182'), (50000, 'U+2187'), (100000, 'U+2188')]
+numeral_lookup = [(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D'), (1000, 'M'), (5000, '\u2181'),
+                  (10000, '\u2182'), (50000, '\u2187'), (100000, '\u2188')]
 
 
-def place_rom_sym(symbol, value):
-    result = []
-    if result: # check if empty list; if so add right away
-        result.append(symbol)
-    # for item in numeral_lookup:
-    #     if value < :
-    #         result.append(symbol)
-    result.append(symbol) # TMP
-        # else result.insert(0, symbol) ?
-    # or insert here?
-    # check then append
-    # check then insert index 0
-    # rules for adding symbols
-    return str(result)
-
-# num
-# num // lowest num but bigger, for example 400 is smaller than L but bigger or equal than C; C < 400 < L
-# store result in var
-# result * var for amount of symbols
-# num %= current roman numeral
-# if 0; Done
-
-
-def decompose(integer):
-    prev = -1
-    pair = ()
-    while integer != 0:
-        for item in numeral_lookup:
-            if item[0] > integer:
-                break
-            prev = item[0]
-            pair = item
-        result = integer // prev
-        if result != 0:
-            print(pair[1] * result) # place_roam_sym
-            #place_rom_sym(pair[1] * result, integer)
-            integer -= result
-            result %= prev
-    return
-
-
-def long_decompose(integer):
-    number = []
-    prev = -1
-    pair = ()
-    while integer != 0:
-        for item in numeral_lookup:
-            if item[0] > integer:
-                break
-            prev = item[0]
-            pair = item
-        result = integer // prev
-        if result != 0:
-            # print(pair[1] * result) # place_roam_sym
-            # place_rom_sym(pair[1] * result, integer)
-            number.append(pair[1] * result)
-            integer -= prev * result
-            result %= prev
-    return
-
-# if numeber is == next closes up - down from that
-#     is a subtraction of the lesser number from the bigger
-#     decompose numbers out
-# if number is == next closes down + down from that
-#     is an addition of the lesser of the lesser
-#     decompose numbers out
-# else decompose regular and place (not to sure where END?)
-
-
-def is_subtraction(num):
+def is_subtraction(num):  # Logic problem where a subtraction only goes on depth deep. Ex: 4, 40 Good. 10 Bad
     result = []
     prev = (0, 0)
 
@@ -122,31 +53,58 @@ def is_addition(num):
     return False
 
 
+def generate_numeral(num):
+    result = []
+    while num != 0:
+        if num == 1:
+            result.append('I')
+            break
+        prev = (0, 'Q')
+        for item in numeral_lookup:
+            if (num == (item[0] - prev[0])) and (num != (item[0] // 2)):  # Subtraction
+                result.append(prev[1])
+                result.append(item[1])
+                num -= (item[0] - prev[0])
+                break
+            elif num == (item[0] + prev[0]):  # Addition
+                result.append(item[1])
+                result.append(prev[1])
+                num -= (item[0] + prev[0])
+                break
+            prev = item
+        if num == 0:
+            break
+        newPrev = (1, 'I')
+        for newitem in numeral_lookup:
+            if num < newitem[0]:
+                result.append(newPrev[1])
+                num -= newPrev[0]
+                break
+            newPrev = newitem
+    return ''.join(result)
+
+
 def main():
     print("Roman Numeral Generator  Copyright (C) 2017  Elliott Sobek\n"
           "This program comes with ABSOLUTELY NO WARRANTY.\n"
           "This is free software, and you are welcome to redistribute it under certain conditions.\n")
-    try:
-        u_in = int(input("Enter a number: "))
-    except ValueError:
-        print("Invalid input")
-        return
-    # decompose(u_in)
-    if is_subtraction(4):
-        print("Yes1")
-    if is_subtraction(5):
-        print("Yes2")
-    if is_addition(6):
-        print("Yes3")
-    if is_addition(5):
-        print("Yes4")
-    if is_addition(100):
-        print("Yes5")
-    if is_addition(1):
-        print("Yes6")
-    if is_subtraction(1):
-        print("Yes7")
-    # long_decompose(u_in)
+    # try:
+    #     u_in = int(input("Enter a number: "))
+    # except ValueError:
+    #     print("Invalid input")
+    #     return
+    # print(decompose(u_in))
+    #print(generate_numeral(3))
+    #print(generate_numeral(4))
+    #print(generate_numeral(5))
+    #print(generate_numeral(6))
+    #print(generate_numeral(8))
+    print(generate_numeral(39))
+    #print(generate_numeral(73))
+    #print(generate_numeral(124))
+    #print(generate_numeral(578))
+    #print(generate_numeral(9999))
     return
+
 
 main()
