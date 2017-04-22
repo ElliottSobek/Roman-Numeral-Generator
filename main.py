ef15f07f9ffac547f285ab3aa96278f598f11071
ElliottSobek <elliottsobek@gmail.com>
2017-04-22 01:23:21 -0600

    Remove depricated addition & subtraction code

--- main.py
+++ main.py
@@ -11,70 +11,41 @@
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
 
-# test numbers: 3, 4, 6, 39, 73, 124, 578, 9999
-
-numeral_lookup = [(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D'), (1000, 'M'), (5000, '\u2181'),
-                  (10000, '\u2182'), (50000, '\u2187'), (100000, '\u2188')]
-
-
-def is_subtraction(num):
-    result = []
-    item = numeral_lookup
-
-    for i in range(1, len(item), 1):
-        for j in range(i - 1, -1, -1):
-            prev2 = item[j]
-            if (num == (item[i][0] - prev2[0])) and (num != (item[i][0] // 2)):
-                result.append(prev2[1])
-                result.append(item[i][1])
-                num -= (item[i][0] - prev2[0])
-                return True
-    return False
-
-
-def is_addition(num):
-    result = []
-    item = numeral_lookup
-    
-    for i in range(1, len(item), 1):
-        for j in range(i - 1, -1, -1):
-            prev2 = item[j]
-            if num == (item[i][0] + prev2[0]):
-                result.append(item[i][1])
-                result.append(prev2[1])
-                num -= (item[i][0] + prev2[0])
-                return True
-    return False
-
 
 def generate_numeral(num):
+    numeral_lookup = [(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D'), (1000, 'M'), (5000, '\u2181'),
+                      (10000, '\u2182'), (50000, '\u2187'), (100000, '\u2188')]
     result = []
     while num != 0:
         if num == 1:
             result.append('I')
             break
-        prev = (0, 'Q')
-        for item in numeral_lookup:
-            if (num == (item[0] - prev[0])) and (num != (item[0] // 2)):  # Subtraction
-                result.append(prev[1])
-                result.append(item[1])
-                num -= (item[0] - prev[0])
-                break
-            elif num == (item[0] + prev[0]):  # Addition
-                result.append(item[1])
-                result.append(prev[1])
-                num -= (item[0] + prev[0])
-                break
-            prev = item
+
+        for i in range(1, len(numeral_lookup), 1):
+            for j in range(i - 1, -1, -1):
+                prev2 = numeral_lookup[j]
+                if (num == (numeral_lookup[i][0] - prev2[0])) and (num != (numeral_lookup[i][0] // 2)):
+                    result.append(prev2[1])
+                    result.append(numeral_lookup[i][1])
+                    num -= (numeral_lookup[i][0] - prev2[0])
+                    break
+                elif num == (numeral_lookup[i][0] + prev2[0]):
+                    result.append(numeral_lookup[i][1])
+                    result.append(prev2[1])
+                    num -= (numeral_lookup[i][0] + prev2[0])
+                    break
+
         if num == 0:
             break
-        newPrev = (1, 'I')
-        for newitem in numeral_lookup:
-            if num < newitem[0]:
-                result.append(newPrev[1])
-                num -= newPrev[0]
+
+        prev = (1, 'I')
+        for item in numeral_lookup:
+            if num < item[0]:
+                result.append(prev[1])
+                num -= prev[0]
                 break
-            newPrev = newitem
+            prev = item
+
     return ''.join(result)
 
 
@@ -82,24 +53,12 @@ def main():
     print("Roman Numeral Generator  Copyright (C) 2017  Elliott Sobek\n"
           "This program comes with ABSOLUTELY NO WARRANTY.\n"
           "This is free software, and you are welcome to redistribute it under certain conditions.\n")
-    # try:
-    #     u_in = int(input("Enter a number: "))
-    # except ValueError:
-    #     print("Invalid input")
-    #     return
-    # print(decompose(u_in))
-    # print(generate_numeral(3))
-    # print(generate_numeral(4))
-    # print(generate_numeral(5))
-    # print(generate_numeral(6))
-    # print(generate_numeral(8))
-    # print(generate_numeral(39))
-    # print(generate_numeral(73))
-    # print(generate_numeral(124))
-    # print(generate_numeral(578))
-    # print(generate_numeral(9999))
-    print(is_addition(60))
+    try:
+        u_in = int(input("Enter a number: "))
+    except ValueError:
+        print("Invalid input")
+        return
+    print(generate_numeral(u_in))
     return
-
 
 main()
