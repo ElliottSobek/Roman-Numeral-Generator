7c076de38182c02683f1e656636e7f02b82c2106
ElliottSobek <elliottsobek@gmail.com>
2017-04-21 20:25:46 -0600

    Implement second pass on fix for issue 1

--- main.py
+++ main.py
@@ -17,23 +17,11 @@ numeral_lookup = [(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D
                   (10000, '\u2182'), (50000, '\u2187'), (100000, '\u2188')]
 
 
-def is_subtraction(num):  # Logic problem where a subtraction only goes on depth deep. Ex: 4, 40 Good. 10 Bad
+def is_subtraction(num):
     result = []
-    prev = (0, 0)
-
-    if num == 1:
-        return False
-
-    for item in numeral_lookup:
-        if (num == (item[0] - prev[0])) and (num != (item[0] // 2)):
-            result.append(prev[1])
-            result.append(item[1])
-            num -= (item[0] - prev[0])
-            return True
-        prev = item
-    # return result
     item = numeral_lookup
-    for i in range(1, 3, 1):
+
+    for i in range(1, len(numeral_lookup), 1):
         for j in range(i - 1, -1, -1):
             prev2 = item[j]
             if (num == (item[i][0] - prev2[0])) and (num != (item[i][0] // 2)):
@@ -109,7 +97,7 @@ def main():
     #print(generate_numeral(5))
     #print(generate_numeral(6))
     #print(generate_numeral(8))
-    print(generate_numeral(39))
+    # print(generate_numeral(39))
     #print(generate_numeral(73))
     #print(generate_numeral(124))
     #print(generate_numeral(578))
