f73ee5a09c8c91dd97f24c33fcf2093bdd09fd17
Elliott Sobek <elliottsobek@gmail.com>
2017-07-11 01:23:32 -0600

    Implement command line argument logic

--- RomNumGen.py
+++ RomNumGen.py
@@ -11,18 +11,24 @@
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
 
+import sys
+import getopt
+
+num_lookup = [(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D'), (1000, 'M'), (5000, '\u2181'),
+              (10000, '\u2182'), (50000, '\u2187'), (100000, '\u2188')]
+
 
 def recurse_gn(num):
     if num == 0:
         return
 
-    num_lookup = [(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D'), (1000, 'M'), (5000, '\u2181'),
-                  (10000, '\u2182'), (50000, '\u2187'), (100000, '\u2188')]
-
     for f_i in range(1, len(num_lookup), 1):  # f_i: forward index
+
         for b_i in range(f_i - 1, -1, -1):  # b_i: backwards index
             prev_asoc = num_lookup[b_i]
+
             if (num == (num_lookup[f_i][0] - prev_asoc[0])) and (num != (num_lookup[f_i][0] // 2)):
+                print("bob", end='')
                 print(prev_asoc[1] + num_lookup[f_i][1], end='')
                 num -= (num_lookup[f_i][0] - prev_asoc[0])
                 recurse_gn(num)
@@ -35,6 +41,7 @@ def recurse_gn(num):
 
     prev_asoc = (1, 'I')
     for num_asoc in num_lookup:
+
         if num < num_asoc[0]:
             print(prev_asoc[1], end='')
             num -= prev_asoc[0]
@@ -44,15 +51,15 @@ def recurse_gn(num):
 
 
 def generate_numeral(num):
-    num_lookup = [(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D'), (1000, 'M'), (5000, '\u2181'),
-                  (10000, '\u2182'), (50000, '\u2187'), (100000, '\u2188')]
     result = []
 
     while num != 0:
 
         for f_i in range(1, len(num_lookup), 1):  # f_i: forward index
+
             for b_i in range(f_i - 1, -1, -1):  # b_i: backwards index
                 prev_asoc = num_lookup[b_i]
+
                 if (num == (num_lookup[f_i][0] - prev_asoc[0])) and (num != (num_lookup[f_i][0] // 2)):
                     result.append(prev_asoc[1])
                     result.append(num_lookup[f_i][1])
@@ -69,6 +76,7 @@ def generate_numeral(num):
 
         prev_asoc = (1, 'I')
         for num_asoc in num_lookup:
+
             if num < num_asoc[0]:
                 result.append(prev_asoc[1])
                 num -= prev_asoc[0]
@@ -78,16 +86,46 @@ def generate_numeral(num):
     return ''.join(result)
 
 
-def main():
+def main(argv):
+    recurse = False
+
     print("Roman Numeral Generator  Copyright (C) 2017  Elliott Sobek\n"
           "This program comes with ABSOLUTELY NO WARRANTY.\n"
           "This is free software, and you are welcome to redistribute it under certain conditions.\n")
+
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+            print("Usage: python3 RomNumGen.py [-hr]")
+            return
+
     try:
-        usr_in = int(input("Enter a number: "))
-    except ValueError:
-        print("Invalid input")
+        opts = getopt.getopt(argv, "hr")
+    except getopt.GetoptError:
+        print("python3 RomNumGen.py [-hr] <unsigned int>")
         return
-    print(generate_numeral(usr_in))
+
+    for opt in opts:
+        if opt == "-h":
+            print("Usage: python3 RomNumGen.py [-hr] <unsigned int>\n\n"
+                  "\t-h\tHelp\n\n"
+                  "\t-r\tRecursive")
+            return
+        elif opt == "-r":
+            recurse = True
+
+    try:
+        usr_in = int(sys.argv[-1])
+    except ValueError:
+        print("Usage: python3 RomNumGen.py [-hr] <unsigned int>")
+        return
+
+    if usr_in < 1:
+        print("Error: Number is less than 1")
+        return
+
+    if recurse:
+        print(recurse_gn(usr_in))
+    else:
+        print(generate_numeral(usr_in))
     return
 
-main()
+main(sys.argv[1:])
