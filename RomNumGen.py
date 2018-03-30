eb515ce0d342d6d70f3e6e5ae877efcfe123e2c0
Elliott <esobek@bbex.com>
2018-03-30 12:13:52 -0600

    Use more verbose imports & format & init values before they are initialized

--- RomNumGen.py
+++ RomNumGen.py
@@ -14,9 +14,8 @@
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
 
-import sys
-import getopt
-
+from sys import argv, exit
+from getopt import getopt, GetoptError
 from os.path import basename
 
 num_lookup = [(1, 'I'), (5, 'V'), (10, 'X'), (50, 'L'), (100, 'C'), (500, 'D'), (1000, 'M'), (5000, '\u2181'),
@@ -91,37 +90,38 @@ def generate_numeral(num):
     return ''.join(result)
 
 
-def main(argc=len(sys.argv), argv=sys.argv):
-    recurse = False
-
+def main(argc=len(argv), argvs=argv):
     if argc < 2 or argc > 3:
-        print("Usage: python3 " + basename(argv[0]) + " [-hr] <unsigned int>")
-        sys.exit(1)
+        print("Usage: python3 " + basename(argvs[0]) + " [-hr] <unsigned int>")
+        exit(1)
+    opts = ''
 
     try:
-        opts, args = getopt.getopt(argv[1:], "hr")
-    except getopt.GetoptError as e:
+        opts, args = getopt(argvs[1:], "hr")
+    except GetoptError as e:
         print(e.msg)
-        sys.exit(1)
-
+        exit(1)
+    recurse = False
+    
     for opt, args in opts:
         if opt == "-h":
-            print("Usage: python3 " + basename(argv[0]) + " [-hr] <unsigned int>\n\n"
-                                                          "\t-h\tHelp\n\n"
-                                                          "\t-r\tRecursive")
-            sys.exit(1)
+            print("Usage: python3 " + basename(argvs[0]) + " [-hr] <unsigned int>\n\n"
+                                                           "\t-h\tHelp\n\n"
+                                                           "\t-r\tRecursive")
+            exit(1)
         elif opt == "-r":
             recurse = True
+    usr_in = 0
 
     try:
-        usr_in = int(sys.argv[-1])
+        usr_in = int(argvs[-1])
     except ValueError as e:
         print(e)
-        sys.exit(1)
+        exit(1)
 
     if usr_in < 1:
         print("Error: Number is less than 1")
-        sys.exit(1)
+        exit(1)
 
     print("Roman Numeral Generator  Copyright (C) 2018  Elliott Sobek\n"
           "This program comes with ABSOLUTELY NO WARRANTY.\n"
